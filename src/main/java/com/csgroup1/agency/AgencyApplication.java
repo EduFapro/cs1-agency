91047f57ccb7b4b4033f03802226af0c48ebcff2
Eduardo Machado <eduardo.fapro@gmail.com>
2024-04-11 18:28:44 -0300

    First commit

---
+++ src/main/java/com/csgroup1/agency/AgencyApplication.java
@@ -0,0 +1,13 @@
+package com.csgroup1.agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyApplication.class, args);
+	}
+
+}
